cd3c05ab137829241f9ceab77f6fd864e2ff018d
Nivrutti Chavan <chavannivrutti130@gmail.com>
2026-09-05 12:03:09 +0530

    using this code snippet ,direct get csv file from the user

---
+++ CSV/code.py
@@ -0,0 +1,14 @@
+import requests
+from io import StringIO
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/plotly/datasets/master/iris-data.csv"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"
+}
+
+req = requests.get(url, headers=headers)
+data = StringIO(req.text)
+
+df=pd.read_csv(data)
+print(df.head())
